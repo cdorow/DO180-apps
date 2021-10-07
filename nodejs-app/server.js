d7aff4d53f7f348d14172d31c27f271c9e827970
Student User <student@workstation.lab.example.com>
2021-10-07 09:14:11 -0400

    add start module

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod:  mein Pod \n')
+  res.send('Hello World from pod:   workstation.lab.example.com\n')
 });
 
 app.listen(8080, function () {
